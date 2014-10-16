6e04c72831d58f5db3cf365e51dca61c5e5fd587
jerryhe9500 <jerryhe9500@gmail.com>
2014-10-16 22:12:04 +0800

    1.1
    Fix the problem about supervisor.

--- Shadowsocks/Shadowsocks-Configure.sh
+++ Shadowsocks/Shadowsocks-Configure.sh
@@ -5,6 +5,8 @@ pip install shadowsocks
 cd ./Config
 cp shadowsocks.json /etc
 cp shadowsocks.conf /etc/supervisor/conf.d
-cat supervisorconfig /etc/default/supervisor > /etc/default/supervisor
 service supervisor start
-supervisorcrl reload
+supervisorctl reload
+echo Now there has the last thing to do.
+echo Add "ulimit -n 51200" to /etc/default/supervisor
+echo And reload supervisor:"supervisorctl reload"
