905f1235b197384ed2160dbc1db978dc3b4ea911
jerryhe9500 <jerryhe9500@Jerrys-MacBook-Pro.local>
2014-10-27 23:09:46 +0800

    2.1

--- Shadowsocks/Shadowsocks-Configure.sh
+++ Shadowsocks/Shadowsocks-Configure.sh
@@ -5,8 +5,7 @@ pip install shadowsocks
 cd ./Config
 cp shadowsocks.json /etc
 cp shadowsocks.conf /etc/supervisor/conf.d
+echo ulimit -n 51200 >> /etc/default/supervisor
 service supervisor start
+supervisorctl update
 supervisorctl reload
-echo Now there has the last thing to do.
-echo Add "ulimit -n 51200" to /etc/default/supervisor
-echo And reload supervisor:"supervisorctl reload"
